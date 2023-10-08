e6971240425a814c2f2545cb2a17dd03e3529252
Johanne Badin <jbadin.dev@gmail.com>
2023-10-08 23:11:13 +0200

    Premier ajout, version mobile 50%

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let socialNetworks = document.querySelectorAll('.socialNetworks')
+
+for(let sn of socialNetworks) {
+    sn.onmouseover = (e) => {
+        e.target.classList.add('fa-bounce')
+    }
+
+    sn.onmouseleave = (e) => {
+        e.target.classList.remove('fa-bounce')
+    }
+}
