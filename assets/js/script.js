9ba20748fe91837a93bbc0b03154249243728d71
Johanne Badin <jbadin.dev@gmail.com>
2023-10-10 00:07:34 +0200

    Modification style pour respect bonnes pratiques

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,25 +10,19 @@ for (let sn of socialNetworks) {
     }
 }
 
+
 function timelineResize(startSize = 0) {
     let size = startSize
-    let timelineContainer = document.querySelector('.timeline_container')
-    let timelineBubble = document.getElementsByClassName('timeline_bubbles')
     let timelineBar = document.querySelector('.timeline_bar')
     let timelineJob = document.getElementsByClassName('timeline_job');
     for (let jobs of timelineJob) {
         size += jobs.offsetHeight + 50
     }
     timelineBar.style.height = size + 'px'
-    for (let b of timelineBubble) {
-        let pos = size - 10;
-        b.style.top = '-' + pos + 'px'
-        b.nextElementSibling.style.top = '-' + pos + 'px'
-    }
-    timelineContainer.style.height = (size + 100) + 'px'
 }
 
 timelineResize(100);
+
 window.onresize = () => {
     timelineResize();
 }
