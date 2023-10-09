2b34b85d120ecae6b80ceb8b0db31bef062cd26e
Johanne Badin <jbadin.dev@gmail.com>
2023-10-09 22:23:23 +0200

    Normalement timeline ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 let socialNetworks = document.querySelectorAll('.socialNetworks')
 
-for(let sn of socialNetworks) {
+for (let sn of socialNetworks) {
     sn.onmouseover = (e) => {
         e.target.classList.add('fa-bounce')
     }
@@ -8,4 +8,27 @@ for(let sn of socialNetworks) {
     sn.onmouseleave = (e) => {
         e.target.classList.remove('fa-bounce')
     }
+}
+
+function timelineResize(startSize = 0) {
+    let size = startSize
+    let timelineContainer = document.querySelector('.timeline_container')
+    let timelineBubble = document.getElementsByClassName('timeline_bubbles')
+    let timelineBar = document.querySelector('.timeline_bar')
+    let timelineJob = document.getElementsByClassName('timeline_job');
+    for (let jobs of timelineJob) {
+        size += jobs.offsetHeight + 50
+    }
+    timelineBar.style.height = size + 'px'
+    for (let b of timelineBubble) {
+        let pos = size - 10;
+        b.style.top = '-' + pos + 'px'
+        b.nextElementSibling.style.top = '-' + pos + 'px'
+    }
+    timelineContainer.style.height = (size + 100) + 'px'
+}
+
+timelineResize(100);
+window.onresize = () => {
+    timelineResize();
 }
